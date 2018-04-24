27c8553ef72bc71e84ac695f7eaf0bb7c1a804e3
Lucas Heim <lheim@asesconsulting.com>
2018-04-24 11:28:05 -0300

    angular con jquery

--- angular-jquery/src/app/app.component.ts
+++ angular-jquery/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+declare var $;
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+
+  public toggleTitle() {
+    $('.titulo').slideToggle();
+  }
 }
